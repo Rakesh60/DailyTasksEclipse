28b1ad9d4c7b42529826b4fd172c16cfd8fd1ae7
Rakesh60 <rakeshgupta5353@gmail.com>
2024-07-06 22:01:22 +0530

    student class created

--- MavenFirstProject/src/main/java/com/rakesh/beans/Student.java
+++ MavenFirstProject/src/main/java/com/rakesh/beans/Student.java
@@ -4,4 +4,22 @@ public class Student {
 	private String name;
 	private String rollno;
 	private String marks;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRollno() {
+		return rollno;
+	}
+	public void setRollno(String rollno) {
+		this.rollno = rollno;
+	}
+	public String getMarks() {
+		return marks;
+	}
+	public void setMarks(String marks) {
+		this.marks = marks;
+	}
 }
